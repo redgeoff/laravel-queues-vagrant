1e2f9b6e92dc4cdbbe0cfa9651bd4ac34ecb101d
Geoff Cox <redgeoff@gmail.com>
2016-08-16 15:40:47 -0700

    init

--- config/blog.sh
+++ config/blog.sh
@@ -2,11 +2,6 @@
 
 cd /vagrant/app
 
-# Generate new Laravel project called "blog"
-laravel new blog
-
-cd blog
-
 # Create MySQL DB
 mysql -uroot -psecret -e "CREATE DATABASE mydb"
 
@@ -15,11 +10,18 @@ sed -i "s'DB_DATABASE=homestead'DB_DATABASE=mydb'" .env
 sed -i "s'DB_USERNAME=homestead'DB_USERNAME=root'" .env
 sed -i "s'QUEUE_DRIVER=sync'QUEUE_DRIVER=database'" .env
 
-# Create DB migration script
-php artisan queue:table
+# Generate new Laravel project called "blog"
+if [ ! -d "blog" ]; then
+  laravel new blog
 
-# Migrate DB
-php artisan migrate
+  cd blog
 
-# Copy our job to the Laravel project
-cp ../../php/SendEmail.php app/Jobs
+  # Create DB migration script
+  php artisan queue:table
+
+  # Migrate DB
+  php artisan migrate
+
+  # Copy our job to the Laravel project
+  cp ../../php/SendEmail.php app/Jobs
+fi
